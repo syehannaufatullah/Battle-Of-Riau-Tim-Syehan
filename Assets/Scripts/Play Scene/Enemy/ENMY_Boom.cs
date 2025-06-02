2232e538f5e2702ae7d0da1c344557f4b192b597
syehannaufatullah <syehannaufatullah@gmail.com>
2025-06-02 14:27:44 +0700

    peh

--- Assets/Scripts/Play Scene/Enemy/ENMY_Boom.cs
+++ Assets/Scripts/Play Scene/Enemy/ENMY_Boom.cs
@@ -1,122 +1,128 @@
-﻿// using UnityEngine;
+﻿    // using UnityEngine;
 
-// public class ENMY_Boom : MonoBehaviour
-// {
-//     public GameObject Barrel, Explosion;
+    // public class ENMY_Boom : MonoBehaviour
+    // {
+    //     public GameObject Barrel, Explosion;
 
-//     private AudioSource source;
+    //     private AudioSource source;
 
-//     [SerializeField]
-//     private float range;
+    //     [SerializeField]
+    //     private float range;
 
-//     private void Awake()
-//     {
-//         Barrel.SetActive(true);
-//         Explosion.SetActive(false);
+    //     private void Awake()
+    //     {
+    //         Barrel.SetActive(true);
+    //         Explosion.SetActive(false);
 
-//         source = GetComponent<AudioSource>();
-//     }
+    //         source = GetComponent<AudioSource>();
+    //     }
 
-//     public void Explode()
-//     {
-//         Barrel.SetActive(false);
-//         Explosion.SetActive(true);
+    //     public void Explode()
+    //     {
+    //         Barrel.SetActive(false);
+    //         Explosion.SetActive(true);
 
-//         source.Play();
-//         this.enabled = false;
-//     }
+    //         source.Play();
+    //         this.enabled = false;
+    //     }
 
-//     private void OnTriggerEnter(Collider other)
-//     {
-//         if (other.CompareTag("Arrow") || (other.CompareTag("Cannon Ball")))
-//         {
-//             Explode();
-//             Destroy(other.gameObject);
-//             Die();
-//         }
-//     }
+    //     private void OnTriggerEnter(Collider other)
+    //     {
+    //         if (other.CompareTag("Arrow") || (other.CompareTag("Cannon Ball")))
+    //         {
+    //             Explode();
+    //             Destroy(other.gameObject);
+    //             Die();
+    //         }
+    //     }
 
-//     private void OnDrawGizmos()
-//     {
-//         Gizmos.DrawWireSphere(transform.position, range);
-//     }
+    //     private void OnDrawGizmos()
+    //     {
+    //         Gizmos.DrawWireSphere(transform.position, range);
+    //     }
 
-//     void Die()
-//     {
-//         Invoke(nameof(DestroyBoom), 2f);
-//     }
+    //     void Die()
+    //     {
+    //         Invoke(nameof(DestroyBoom), 2f);
+    //     }
 
-//     void DestroyBoom()
-//     {
-//         Destroy(gameObject);
-//     }
-// }
-using UnityEngine;
+    //     void DestroyBoom()
+    //     {
+    //         Destroy(gameObject);
+    //     }
+    // }
+    using UnityEngine;
 
-public class ENMY_Boom : MonoBehaviour
-{
-    public GameObject Barrel;
-    public GameObject[] Explosions; // Array untuk menampung banyak efek ledakan
-
-    private AudioSource source;
-
-    [SerializeField]
-    private float range;
-
-    private void Awake()
+    public class ENMY_Boom : MonoBehaviour
     {
-        Barrel.SetActive(true);
-        
-        // Pastikan semua ledakan dinonaktifkan saat memulai
-        foreach (var explosion in Explosions)
+        public GameObject Barrel;
+        public GameObject[] Explosions; // Array untuk menampung banyak efek ledakan
+        public GameObject SmokeEffectPrefab; // prefab efek asap
+
+        private AudioSource source;
+
+        [SerializeField]
+        private float range;
+
+        private void Awake()
         {
-            explosion.SetActive(false);
+            Barrel.SetActive(true);
+            
+            // Pastikan semua ledakan dinonaktifkan saat memulai
+            foreach (var explosion in Explosions)
+            {
+                explosion.SetActive(false);
+            }
+
+            source = GetComponent<AudioSource>();
         }
 
-        source = GetComponent<AudioSource>();
-    }
-
-    public void Explode()
-    {
-        Barrel.SetActive(false);
-
-        // Aktifkan semua efek ledakan di dalam array
-        foreach (var explosion in Explosions)
+        public void Explode()
         {
-            explosion.SetActive(true);
+            Barrel.SetActive(false);
+
+            // Aktifkan semua efek ledakan di dalam array
+            foreach (var explosion in Explosions)
+            {
+                explosion.SetActive(true);
+            }
+
+            // Mainkan efek suara
+            if (source != null)
+            {
+                source.Play();
+            }
+
+            if (SmokeEffectPrefab != null)
+            {
+                Instantiate(SmokeEffectPrefab, transform.position, Quaternion.identity);
+            }
+
+            this.enabled = false;
         }
 
-        // Mainkan efek suara
-        if (source != null)
+        private void OnTriggerEnter(Collider other)
         {
-            source.Play();
+            if (other.CompareTag("Arrow") || (other.CompareTag("Cannon Ball")))
+            {
+                Explode();
+                Destroy(other.gameObject);
+                Die();
+            }
         }
 
-        this.enabled = false;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Arrow") || (other.CompareTag("Cannon Ball")))
+        private void OnDrawGizmos()
         {
-            Explode();
-            Destroy(other.gameObject);
-            Die();
+            Gizmos.DrawWireSphere(transform.position, range);
+        }
+
+        void Die()
+        {
+            Invoke(nameof(DestroyBoom), 2f);
+        }
+
+        void DestroyBoom()
+        {
+            Destroy(gameObject);
         }
     }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireSphere(transform.position, range);
-    }
-
-    void Die()
-    {
-        Invoke(nameof(DestroyBoom), 2f);
-    }
-
-    void DestroyBoom()
-    {
-        Destroy(gameObject);
-    }
-}
